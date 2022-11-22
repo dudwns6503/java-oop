10603267a005297a78814d7a6727418d8eb547c7
yeongjun <dudwns650395@gmail.com>
2022-11-22 14:20:50 +0900

    Update #3 - Main
    변수 클래스를 인터페이스로 변경
    #3 마무리.

--- src/main/java/com/yeong/Main.java
+++ src/main/java/com/yeong/Main.java
@@ -1,12 +1,13 @@
 package com.yeong;
 
-import com.yeong.logic.BubbleSort;
+import com.yeong.logic.JavaSort;
+import com.yeong.logic.Sort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+        Sort<String> sort = new JavaSort<>();
 
         System.out.println("[result] " + sort.sort(Arrays.asList(args)));
     }
